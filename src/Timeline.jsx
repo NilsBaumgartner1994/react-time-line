6cf966d473e70b2631c93e99c987dc19f2300b1f
NilsBaumgartner1994 <33295329+NilsBaumgartner1994@users.noreply.github.com>
2019-10-16 22:20:00 +0200

    Update Timeline.jsx

--- src/Timeline.jsx
+++ src/Timeline.jsx
@@ -17,7 +17,7 @@ function getFormattedData(items) {
     const dateStr = date.format("DD MMM YYYY");
     const list = activities[dateStr] || [];
     list.push({
-      time: "Test",
+      time: date.format("HH:MM"),
       text,
       key: index,
     });
